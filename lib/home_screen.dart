b5dc337eb096c6be6ee71fb9b6a655535fe781d7
Awaizkazi <Owaizkazi789@gmail.com>
2022-09-23 12:20:19 +0530

    c

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:api_integration/Models/PostsModel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,12 +12,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi() async {
+    final res = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(res.body.toString());
+    if (res.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('API Integration'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text('Loading Data');
+                } else {
+                  return ListView.builder(
+                      itemBuilder: (BuildContext context, index) {
+                    return Card();
+                  });
+                }
+              }),
+        ],
       ),
     );
   }
