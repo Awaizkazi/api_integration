8455d61c3f7865ae2019a4edd0a10b97b4913247
Awaizkazi <Owaizkazi789@gmail.com>
2022-09-24 18:48:50 +0530

    c

--- lib/question_1.dart
+++ lib/question_1.dart
@@ -17,66 +17,92 @@ class _QuestionOneState extends State<QuestionOne> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        color: Colors.blue[300],
-        child: Card(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter a Number',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Card(
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              shadowColor: Colors.black,
+              elevation: 20,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter a Number',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      controller: num1controller,
                     ),
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: num1controller,
-                ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        int mul = int.parse(num1controller.text) * 3;
+                        result = mul.toString();
+                        // result = mul2.toString();
+                      });
+                    },
+                    child: Text('*3'),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.indigo),
+                  ),
+                  Text('Result  : $result'),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    int mul = int.parse(num1controller.text) * 3;
-                    // int mul2 = int.parse(num2controller.text) * 4;
-                    // int mul2 = int.parse(num1controller.text) * 4;
-                    result = mul.toString();
-                    // result = mul2.toString();
-                  });
-                },
-                child: Text('*3'),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              Text('Result  : $result'),
-              // Text('Result * 4: $res'),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter a Number',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
+              shadowColor: Colors.black,
+              elevation: 20,
+              color: Colors.pink[200],
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter a Number',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      controller: num1controller,
                     ),
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: num1controller,
-                ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        // int mul = int.parse(num1controller.text) * 3;
+                        int mul2 = int.parse(num1controller.text) * 4;
+                        // int mul2 = int.parse(num1controller.text) * 4;
+                        result = mul2.toString();
+                        // res = mul.toString();
+                      });
+                    },
+                    child: Text('*4'),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink[400]),
+                  ),
+                  Text('Result : $result'),
+                  // Text('Result * 4 : $res'),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    // int mul = int.parse(num1controller.text) * 3;
-                    int mul2 = int.parse(num1controller.text) * 4;
-                    // int mul2 = int.parse(num1controller.text) * 4;
-                    result = mul2.toString();
-                    // res = mul.toString();
-                  });
-                },
-                child: Text('*4'),
-              ),
-              Text('Result : $result'),
-              // Text('Result * 4 : $res'),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
